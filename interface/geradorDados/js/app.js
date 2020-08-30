1526e5ec3b3393ed2c0b144c91a396a03cc1b0b6
Rafael Faustini <contato@rafaelfaustini.com.br>
2020-08-30 16:07:38 -0300

    Geração de dados dia a dia

---
+++ interface/geradorDados/js/app.js
@@ -0,0 +1,10 @@
+appVue = new Vue({
+    el: '#appVue',
+    data: {
+        data: new Date().toISOString().slice(0, 10),
+        datas: [],
+        estressado: false
+    },
+    mounted(){
+    }
+  })
--- interface/geradorDados/js/canvas.js
+++ interface/geradorDados/js/canvas.js
@@ -1,8 +1,9 @@
 dados = []
 function setup() {
-    createCanvas(602, 369); 
+    c = createCanvas(602, 369);
+    c.parent("canvaswrapper") 
     input = createFileInput(handleFile);
-    input.position(10, height+ 10);
+    input.parent("fileWrapper")
     img = loadImage("img/fundo.jpg");
   }
 img = null
@@ -19,35 +20,47 @@ function draw() {
     background(img);
     fill(color(255, 204, 0, .7));
     ellipse(mouseX, mouseY, 10, 10);
+    var contador=0;
     for(let i = 0; i< dados.length; i++){
         element = dados[i]
-        switch(i){
-            case 0:
+        if(dados[i].date == appVue.data){
+            chaves = dados.map(el => el.date)
+            if(contador == 0){
                 fill(color(0, 255, 0))
-                break;
-            case dados.length-1:
+            } else if (i >= chaves.lastIndexOf(dados[i].date)){
                 fill(color(255, 0, 0))
-                break;
-            default:
+            } else {
                 fill(color(255, 204, 0));
-                break
-        }
+            }
         circle(element.x, element.y, 10, 10) 
 
-        if(i+1 < dados.length){
+        if(i+1 < dados.length && dados[i].date == dados[i+1].date){
             line(element.x, element.y, dados[i+1].x, dados[i+1].y);
             fill(color(0, 0, 0));
         }
-
+        contador+=1
+    } else{
+        contador=0
+    }
     }
 }
 
 function doubleClicked() {
 
+    isDataNova = !dados.some(el => el.date == appVue.data)
+    if(isDataNova){
+        appVue.datas.push(appVue.data)
+        appVue.datas.sort()
+    }
     dados.push({
+        date: appVue.data,
         x: mouseX,
-        y: mouseY
+        y: mouseY,
+        stress: appVue.estressado || false
     })
+    dados.sort(function(a,b){
+        return new Date(b.date) - new Date(a.date)
+    });
 }
 function keyPressed() {
     switch(keyCode){
@@ -63,7 +76,20 @@ function keyPressed() {
     return false;
   }
 function salvarDados(){
+    dados.sort(function(a,b){
+        return new Date(b.date) - new Date(a.date)
+    });
     json = JSON.stringify(dados);
     console.log(json)
     saveJSON(JSON.stringify(dados), 'dados.json');
 }
+$("#date").change(function() {
+    dados.sort(function(a,b){
+        redraw()
+        return new Date(b.date) - new Date(a.date)
+    });
+    let a = dados.find(obj => obj.date === appVue.data)
+    if(a){
+    appVue.estressado = a.stress
+    }
+});
