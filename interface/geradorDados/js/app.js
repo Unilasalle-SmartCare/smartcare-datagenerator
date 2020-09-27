c697222d51a20528d586d62f6e7f7ea944dc5b1d
Rafael Faustini <contato@rafaelfaustini.com.br>
2020-09-27 09:38:33 -0300

    Melhoria na confiabilidade
    Durante a alteração do dataset, me deparei com alguns problemas que quebravam todo o código e assim todo o progresso era perdido, portanto, neste commit visei o tratamento de exceções com alguns códigos de erro para facilitar a detecção.

--- interface/geradorDados/js/app.js
+++ interface/geradorDados/js/app.js
@@ -20,16 +20,20 @@ appVue = new Vue({
             qtdNormal: 0,
         },
         estatisticaAtual: { // Estatísticas da data atual
-            qtdRegistros : 0,
+            qtdRegistros: 0,
             qtdStress: 0,
             qtdNormal: 0,
+        },
+        erro: {
+            show: false,
+            texto: ""
         }
     },
     methods: {
-        onTrocaData(){
-            if(dados){
+        onTrocaData() {
+            if (dados) {
                 let a = dados.find(obj => obj.date === appVue.date)
-                if(a){
+                if (a) {
                     appVue.estressado = a.stress
                 }
                 appVue.estatisticaAtual.qtdStress = 0;
@@ -61,151 +65,193 @@ appVue = new Vue({
                 appVue.estatisticaHora.qtdHoras = _.uniqBy(dados, 'date').length;
             }
         },
-        setStress(){
-            debugger
-            if(dados){
-                for(i=0; i<dados.length; i++){
-                    if(dados[i].date === appVue.data){
-                        dados[i].stress = appVue.estressado;
+        setStress() {
+            try {
+                if (dados) {
+                    for (i = 0; i < dados.length; i++) {
+                        if (dados[i].date === appVue.data) {
+                            dados[i].stress = appVue.estressado;
+                        }
                     }
                 }
+                this.onTrocaData()
+            } catch (error) {
+                this.geraErro(`Houve um erro interno (0x85) - ${error}`)
             }
         },
-        datasOrdenadas(){
-            if(this.datas.length){
-                var index = this.datas.indexOf(this.data);
-                var d = this.datas.slice();
-                if (index > -1) {
-                    d.splice(index, 1);
-                    return d.sort()
-                } else{
-                    return this.datas
-                }
-            } else {
-                return []
-            }
+        geraErro(erro) {
+            this.erro.texto = erro;
+            this.erro.show = true;
         },
-        save(){
-            swal({
-                title: "Deseja salvar?",
-                text: "As atividades salvas podem ser carregadas posteriormente com o arquivo JSON gerado!",
-                icon: "info",
-                buttons: true,
-                dangerMode: false,
-              })
-              .then((salvar) => {
-                if (salvar) {
-                    if(dados){
-                        dados = _.sortBy(dados, 'date');
-                        json = JSON.stringify(dados);
-                        saveJSON(JSON.stringify(dados), 'dados.json');
+        checkStress(data) {
+            try {
+                var dataS = _.filter(dados, function(o) {
+                    if (o.date == data) return o;
+                });
+                if (dataS.length) {
+                    if (dataS[0].stress) {
+                        return 0
+                    } else {
+                        return 1
                     }
-                  swal("Pronto! O arquivo foi salvo com sucesso", {
-                    icon: "success",
-                  });
                 } else {
-                    return;
+                    return 2;
                 }
-              });
-            e.preventDefault();
-            return false;
+            } catch (error) {
+                this.geraErro(`Houve um erro interno (0x27) - ${error}`)
+            }
         },
-        uploadFundo(){
+        datasOrdenadas() {
+            try {
+                if (this.datas.length) {
+                    var index = this.datas.indexOf(this.data);
+                    var d = this.datas.slice();
+                    if (index > -1) {
+                        d.splice(index, 1);
+                        return d.sort()
+                    } else {
+                        return this.datas
+                    }
+                } else {
+                    return []
+                }
+            } catch (error) {
+                this.geraErro(`Erro ao ordenadar as datas (0xb1) - ${error}`)
+            }
+        },
+        save() {
+            try {
+                swal({
+                        title: "Deseja salvar?",
+                        text: "As atividades salvas podem ser carregadas posteriormente com o arquivo JSON gerado!",
+                        icon: "info",
+                        buttons: true,
+                        dangerMode: false,
+                    })
+                    .then((salvar) => {
+                        if (salvar) {
+                            if (dados) {
+                                dados = _.sortBy(dados, 'date');
+                                json = JSON.stringify(dados);
+                                saveJSON(JSON.stringify(dados), 'dados.json');
+                            }
+                            swal("Pronto! O arquivo foi salvo com sucesso", {
+                                icon: "success",
+                            });
+                        } else {
+                            return;
+                        }
+                    });
+                e.preventDefault();
+                return false;
+            } catch (error) {
+                this.geraErro(erro)
+            }
+        },
+        uploadFundo() {
             $('#fileInputImg').trigger('click');
         },
-        deletarDados(){
-            swal({
-                title: "Tem certeza?",
-                text: "Uma vez deletado, você não pode recuperar os dados de atividade",
-                icon: "warning",
-                buttons: {
-                    cancel: "Cancelar",
-                    deletarHora: {
-                        text: "Deletar Hora",
-                        value: "deletahora"
-                    },
-                    deletarTudo: {
-                        text: "Deletar Tudo",
-                        value: "deletatudo"
-                    }
-                },
-                dangerMode: true,
-              })
-              .then((value) => {
-                  switch(value){
-                    case "deletahora":
-                        swal(`Os dados de atividade do idoso de ${appVue.data} foram deletados!`, {
-                            icon: "success",
-                        });
-                        _.remove(dados, {date: appVue.data});
-                        appVue.qtdRegistros = 0
-                        appVue.qtdStress = 0
-                        appVue.qtdNormal = 0
-                        appVue.datas.sort()
-                        for(i in dados){
-                            if(!isNaN(parseInt(i))){
-                            appVue.qtdRegistros++
-                            if(dados[i].stress){
-                                appVue.qtdStress++
-                            } else {
-                                appVue.qtdNormal++ 
+        deletarDados() {
+            try {
+                swal({
+                        title: "Tem certeza?",
+                        text: "Uma vez deletado, você não pode recuperar os dados de atividade",
+                        icon: "warning",
+                        buttons: {
+                            cancel: "Cancelar",
+                            deletarHora: {
+                                text: "Deletar Hora",
+                                value: "deletahora"
+                            },
+                            deletarTudo: {
+                                text: "Deletar Tudo",
+                                value: "deletatudo"
                             }
-                            }
-                        }
-                        appVue.mediaPontos = (appVue.qtdRegistros / dados.map(x => x.date).unique().length).toFixed(2)
-                        if(!isFinite(appVue.mediaPontos)){
-                            appVue.mediaPontos = 0
-                        }
-                        let pontos = _.countBy(dados, 'date');
-                        let min = _.minBy(_.entries(pontos), 1)
-                        let max = _.maxBy(_.entries(pontos), 1)
-                        if(min.length){
-                            appVue.minPontos = min[1]
-                        }
-                        if(max.length){
-                            appVue.maxPontos = max[1]
-                        }
-            
-                        appVue.estatisticaHora.qtdStress = _.filter(_.uniqBy(dados, 'date'), function(o) {
-                            if (o.stress === true) return o;
-                        }).length;
-                        appVue.estatisticaHora.qtdNormal = _.filter(_.uniqBy(dados, 'date'), function(o) {
-                            if (o.stress === false) return o;
-                        }).length;
-                        appVue.estatisticaHora.qtdHoras = _.uniqBy(dados, 'date').length;
-                        this.onTrocaData()
+                        },
+                        dangerMode: true,
+                    })
+                    .then((value) => {
+                        switch (value) {
+                            case "deletahora":
+                                swal(`Os dados de atividade do idoso de ${appVue.data} foram deletados!`, {
+                                    icon: "success",
+                                });
+                                _.remove(dados, {
+                                    date: appVue.data
+                                });
+                                appVue.qtdRegistros = 0
+                                appVue.qtdStress = 0
+                                appVue.qtdNormal = 0
+                                appVue.datas.sort()
+                                for (i in dados) {
+                                    if (!isNaN(parseInt(i))) {
+                                        appVue.qtdRegistros++
+                                        if (dados[i].stress) {
+                                            appVue.qtdStress++
+                                        } else {
+                                            appVue.qtdNormal++
+                                        }
+                                    }
+                                }
+                                appVue.mediaPontos = (appVue.qtdRegistros / dados.map(x => x.date).unique().length).toFixed(2)
+                                if (!isFinite(appVue.mediaPontos)) {
+                                    appVue.mediaPontos = 0
+                                }
+                                let pontos = _.countBy(dados, 'date');
+                                let min = _.minBy(_.entries(pontos), 1)
+                                let max = _.maxBy(_.entries(pontos), 1)
+                                if (min.length) {
+                                    appVue.minPontos = min[1]
+                                }
+                                if (max.length) {
+                                    appVue.maxPontos = max[1]
+                                }
 
-                        this.datas = this.datas.filter(function(e) { return e !== appVue.data })
+                                appVue.estatisticaHora.qtdStress = _.filter(_.uniqBy(dados, 'date'), function(o) {
+                                    if (o.stress === true) return o;
+                                }).length;
+                                appVue.estatisticaHora.qtdNormal = _.filter(_.uniqBy(dados, 'date'), function(o) {
+                                    if (o.stress === false) return o;
+                                }).length;
+                                appVue.estatisticaHora.qtdHoras = _.uniqBy(dados, 'date').length;
+                                this.onTrocaData()
 
-                        break;
-                    case "deletatudo":
-                        swal("Os dados de atividade do idoso foram deletados!", {
-                            icon: "success",
-                          });
-                          dados = [];
-                          appVue.qtdRegistros = 0;
-                          appVue.qtdStress = 0;
-                          appVue.qtdNormal = 0;
-                          appVue.estatisticaAtual.qtdStress = 0;
-                          appVue.estatisticaAtual.qtdNormal = 0;
-                          appVue.estatisticaAtual.qtdRegistros = 0;
-                        break;
-                  }
+                                this.datas = this.datas.filter(function(e) {
+                                    return e !== appVue.data
+                                })
 
-              });
+                                break;
+                            case "deletatudo":
+                                swal("Os dados de atividade do idoso foram deletados!", {
+                                    icon: "success",
+                                });
+                                dados = [];
+                                appVue.qtdRegistros = 0;
+                                appVue.qtdStress = 0;
+                                appVue.qtdNormal = 0;
+                                appVue.estatisticaAtual.qtdStress = 0;
+                                appVue.estatisticaAtual.qtdNormal = 0;
+                                appVue.estatisticaAtual.qtdRegistros = 0;
+                                break;
+                        }
+
+                    });
+            } catch (error) {
+                this.geraErro(`Houve um erro interno (0xd7) - ${error}`)
+            }
         },
-        uploadJson(){
+        uploadJson() {
             $('#fileInputJson').trigger('click');
         }
     },
     computed: {
-        date(){
-                let date = moment(this.data, "DD/MM/YYYY HH:00");
-                if(date.isValid()){
-                    return date.format("DD/MM/YYYY HH:00")
-                } else{
-                    return null
-                }
+        date() {
+            let date = moment(this.data, "DD/MM/YYYY HH:00");
+            if (date.isValid()) {
+                return date.format("DD/MM/YYYY HH:00")
+            } else {
+                return null
+            }
         },
     },
-  })
+})
--- interface/geradorDados/js/canvas.js
+++ interface/geradorDados/js/canvas.js
@@ -1,14 +1,14 @@
 dados = []
 img = null
 Array.prototype.unique = function() {
-    return this.filter(function (value, index, self) {
-      return self.indexOf(value) === index;
+    return this.filter(function(value, index, self) {
+        return self.indexOf(value) === index;
     });
-  };
+};
+
 function setup() {
-    
     c = createCanvas(602, 369);
-    c.parent("canvaswrapper") 
+    c.parent("canvaswrapper")
     c.doubleClicked(doubleClick)
     inputFundo = createFileInput(handleImage);
     inputFundo.id("fileInputImg")
@@ -20,33 +20,39 @@ function setup() {
     inputJson.parent("jsonWrapper");
 
     img = loadImage("img/fundo.jpg");
-  }
-  function handleImage(file) {
-    if (file.type === 'image') {
-      img = loadImage(file.data);
-    } else {
-      img = null;
+}
+
+function handleImage(file) {
+    try {
+        if (file.type === 'image') {
+            img = loadImage(file.data);
+        } else {
+            img = null;
+        }
+    } catch (error) {
+        appVue.geraErro(`Houve um erro ao carregar a imagem (0x16) - ${error}`)
     }
-  }
-  function handleJSON(file){
-      try {
+}
+
+function handleJSON(file) {
+    try {
         if (file.type === 'application') {
             json = JSON.parse(file.data);
             dados = json;
             datas = dados.filter((value, index, self) => {
                 return self.findIndex(v => v.date === value.date) === index;
             })
-            appVue.datas= [];
-            for (i in datas){
+            appVue.datas = [];
+            for (i in datas) {
                 appVue.datas.push(datas[i].date)
             }
             appVue.datas.sort()
-            for(i in dados){
+            for (i in dados) {
                 appVue.qtdRegistros++
-                if(dados[i].stress){
+                if (dados[i].stress) {
                     appVue.qtdStress++
                 } else {
-                    appVue.qtdNormal++ 
+                    appVue.qtdNormal++
                 }
             }
             appVue.mediaPontos = (appVue.qtdRegistros / dados.map(x => x.date).unique().length).toFixed(2)
@@ -61,125 +67,137 @@ function setup() {
                 if (o.stress === false) return o;
             }).length;
             appVue.estatisticaHora.qtdHoras = _.uniqBy(dados, 'date').length;
-           }
-      } catch (error) {
-          console.log(error)
-      }
-  }
+        }
+    } catch (error) {
+        appVue.geraErro(`Houve um erro ao carregar os dados (0x69) - ${error}`)
+    }
+}
 
-function drawCaminho(){
-    var contador=0;
+function drawCaminho() {
+    var contador = 0;
     try {
-        for(let i = 0; i< dados.length; i++){
+        for (let i = 0; i < dados.length; i++) {
             element = dados[i]
-            if(dados[i].date == appVue.date){
+            if (dados[i].date == appVue.date) {
                 dados[i].stress = appVue.estressado;
                 chaves = dados.map(el => el.date)
-                
-                if(contador == 0){
+
+
+
+                if (i + 1 < dados.length && dados[i].date == dados[i + 1].date) {
+                    fill(color(38, 50, 56));
+                    strokeWeight(2);
+                    line(element.x, element.y, dados[i + 1].x, dados[i + 1].y);
+
+                }
+                if (contador == 0) {
                     strokeWeight(1.5);
-                    stroke(27,94,32);
-                    fill(color(205,220,57))
-                } else if (i >= chaves.lastIndexOf(dados[i].date)){
+                    stroke(27, 94, 32);
+                    fill(color(205, 220, 57))
+                } else if (i >= chaves.lastIndexOf(dados[i].date)) {
                     strokeWeight(1.5);
-                    stroke(213,0,0);
-                    fill(color(244,67,54))
+                    stroke(213, 0, 0);
+                    fill(color(244, 67, 54))
                 } else {
                     strokeWeight(1.5);
-                    stroke(230,81,0);
-                    fill(color(255,238,88));
+                    stroke(230, 81, 0);
+                    fill(color(255, 238, 88));
                 }
-                
-            circle(element.x, element.y, 12, 12) 
-            strokeWeight(1);
-            stroke(0);
-            if(i+1 < dados.length && dados[i].date == dados[i+1].date){
-                strokeWeight(2);
-                line(element.x, element.y, dados[i+1].x, dados[i+1].y);
-                fill(color(38,50,56));
+                circle(element.x, element.y, 12, 12)
+                strokeWeight(1);
+
+                if (appVue.estressado) {
+                    strokeWeight(2);
+                    stroke(244, 67, 54)
+                } else {
+                    strokeWeight(2);
+                    stroke(0);
+                }
+                contador += 1
+            } else {
+                contador = 0
             }
-            contador+=1
-            } else{
-            contador=0
-            }
-        } 
+        }
     } catch (error) {
-        console.log(error)
+        appVue.geraErro(`Houve um erro ao renderizar os caminhos (0x06) - ${error}`)
     }
 
 }
-  
+
 function draw() {
     background(img);
     fill(color(255, 204, 0, .7));
-    ellipse(mouseX, mouseY, 10, 10);
+    ellipse(mouseX, mouseY, 12, 12);
     drawCaminho()
 }
 
-function adicionaPonto(data, x, y, stress){
-    dados.push({
-        date: data,
-        x: x,
-        y: y,
-        stress: stress || false
-    })
-  dados =  _.sortBy(dados, 'date');
-  appVue.qtdRegistros++
-  appVue.mediaPontos = (appVue.qtdRegistros / dados.map(x => x.date).unique().length).toFixed(2)
-  let pontos = _.countBy(dados, 'date');
-  appVue.minPontos = _.minBy(_.entries(pontos), 1)[1]
-  appVue.maxPontos = _.maxBy(_.entries(pontos), 1)[1]
+function adicionaPonto(data, x, y, stress) {
+    try {
+        dados.push({
+            date: data,
+            x: x,
+            y: y,
+            stress: stress || false
+        })
+        dados = _.sortBy(dados, 'date');
+        appVue.qtdRegistros++
+        appVue.mediaPontos = (appVue.qtdRegistros / dados.map(x => x.date).unique().length).toFixed(2)
+        let pontos = _.countBy(dados, 'date');
+        appVue.minPontos = _.minBy(_.entries(pontos), 1)[1]
+        appVue.maxPontos = _.maxBy(_.entries(pontos), 1)[1]
 
-  appVue.qtdStress = 0;
-  appVue.qtdNormal = 0;
-  appVue.estatisticaAtual.qtdStress = 0;
-  appVue.estatisticaAtual.qtdNormal = 0;
-  appVue.estatisticaAtual.qtdRegistros = 0;
+        appVue.qtdStress = 0;
+        appVue.qtdNormal = 0;
+        appVue.estatisticaAtual.qtdStress = 0;
+        appVue.estatisticaAtual.qtdNormal = 0;
+        appVue.estatisticaAtual.qtdRegistros = 0;
 
 
-  for(i in dados){
-    if(!isNaN(parseInt(i))){
-    if(dados[i].stress){
-        appVue.qtdStress++
-        if(dados[i].date == appVue.data){
-            appVue.estatisticaAtual.qtdStress++
+        for (i in dados) {
+            if (!isNaN(parseInt(i))) {
+                if (dados[i].stress) {
+                    appVue.qtdStress++
+                    if (dados[i].date == appVue.data) {
+                        appVue.estatisticaAtual.qtdStress++
+                    }
+                } else {
+
+                    appVue.qtdNormal++
+                    if (dados[i].date == appVue.data) {
+                        appVue.estatisticaAtual.qtdNormal++
+                    }
+                }
+            }
         }
-    } else {
-        
-        appVue.qtdNormal++ 
-        if(dados[i].date == appVue.data){
-           appVue.estatisticaAtual.qtdNormal++ 
-        }
+
+        var estatisticaAtualPontos = _.filter(dados, function(o) {
+            if (o.date === appVue.data) return o;
+        });
+        appVue.estatisticaAtual.qtdRegistros = estatisticaAtualPontos.length || 0;
+
+
+        appVue.estatisticaHora.qtdStress = _.filter(_.uniqBy(dados, 'date'), function(o) {
+            if (o.stress === true) return o;
+        }).length;
+        appVue.estatisticaHora.qtdNormal = _.filter(_.uniqBy(dados, 'date'), function(o) {
+            if (o.stress === false) return o;
+        }).length;
+        appVue.estatisticaHora.qtdHoras = _.uniqBy(dados, 'date').length;
+    } catch (error) {
+        appVue.geraErro(`Houve um erro ao adicionar um ponto (0x9e) - ${error}`)
     }
-    }
-  }
-
-  var estatisticaAtualPontos = _.filter(dados, function(o) {
-    if (o.date === appVue.data) return o;
-});
-  appVue.estatisticaAtual.qtdRegistros = estatisticaAtualPontos.length || 0;
-
-  
-  appVue.estatisticaHora.qtdStress = _.filter(_.uniqBy(dados, 'date'), function(o) {
-    if (o.stress === true) return o;
-}).length;
-appVue.estatisticaHora.qtdNormal = _.filter(_.uniqBy(dados, 'date'), function(o) {
-    if (o.stress === false) return o;
-}).length;
-appVue.estatisticaHora.qtdHoras = _.uniqBy(dados, 'date').length;
-
 }
 
 function doubleClick() {
     let stress = appVue.estressado
-    if(appVue.date && mouseX && mouseY && typeof stress === "boolean") {
+    if (appVue.date && mouseX && mouseY && typeof stress === "boolean") {
         isDataNova = !dados.some(el => el.date == appVue.date)
-        if(isDataNova){
+        if (isDataNova) {
             appVue.datas.push(appVue.date)
             appVue.datas.sort()
         }
         adicionaPonto(appVue.date, mouseX, mouseY, stress)
-    } else{
+    } else {
         console.log(appVue.date)
         console.log(mouseX)
         console.log(mouseY)
@@ -188,62 +206,35 @@ function doubleClick() {
     return false;
 }
 
-function keyPressed() {
-    switch(keyCode){
-        case BACKSPACE:
-            swal({
-                title: "Tem certeza?",
-                text: "Uma vez deletado, você não pode recuperar os dados de atividade",
-                icon: "warning",
+function salvarDados() {
+    try {
+        dados = _.sortBy(dados, 'date');
+        json = JSON.stringify(dados);
+        saveJSON(JSON.stringify(dados), 'dados.json');
+    } catch (error) {
+        appVue.geraErro(`Houve um erro ao salvar os dados (0xed) - ${error}`)
+    }
+}
+
+$(document).on('keydown', function(e) {
+    if (e.ctrlKey && e.which === 83) {
+        swal({
+                title: "Deseja salvar?",
+                text: "As atividades salvas podem ser carregadas posteriormente com o arquivo JSON gerado!",
+                icon: "info",
                 buttons: true,
-                dangerMode: true,
-              })
-              .then((deletar) => {
-                if (deletar) {
-                  swal("Os dados de atividade do idoso foram deletados!", {
-                    icon: "success",
-                  });
-                  dados = [];
-                  appVue.qtdRegistros = 0;
-                  appVue.qtdStress = 0;
-                  appVue.qtdNormal = 0;
-                  appVue.estatisticaAtual.qtdStress = 0;
-                  appVue.estatisticaAtual.qtdNormal = 0;
-                  appVue.estatisticaAtual.qtdRegistros = 0;
+                dangerMode: false,
+            })
+            .then((salvar) => {
+                if (salvar) {
+                    salvarDados()
+                    swal("Pronto! O arquivo foi salvo com sucesso", {
+                        icon: "success",
+                    });
                 } else {
                     return;
                 }
-              });
-            break;
-    }
-    return false;
-}
-
-function salvarDados(){
-    dados = _.sortBy(dados, 'date');
-    json = JSON.stringify(dados);
-    saveJSON(JSON.stringify(dados), 'dados.json');
-}
-
-$(document).on('keydown', function(e){
-    if(e.ctrlKey && e.which === 83){
-        swal({
-            title: "Deseja salvar?",
-            text: "As atividades salvas podem ser carregadas posteriormente com o arquivo JSON gerado!",
-            icon: "info",
-            buttons: true,
-            dangerMode: false,
-          })
-          .then((salvar) => {
-            if (salvar) {
-              salvarDados()
-              swal("Pronto! O arquivo foi salvo com sucesso", {
-                icon: "success",
-              });
-            } else {
-                return;
-            }
-          });
+            });
         e.preventDefault();
         return false;
     }
